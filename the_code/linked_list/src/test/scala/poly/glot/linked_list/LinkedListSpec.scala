5a434e02b91c1b4c5e6094e16ec6238aad73cd5a
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-08 22:17:31 -0400

    Used the Option type to say that newly initialized LinkedLists have no next element

--- the_code/linked_list/src/test/scala/poly/glot/linked_list/LinkedListSpec.scala
+++ the_code/linked_list/src/test/scala/poly/glot/linked_list/LinkedListSpec.scala
@@ -2,13 +2,21 @@ class LinkedList() {
   def size() : Int = {
     return 0
   }
+
+  def next() : Option[LinkedList] = {
+    return None 
+  }
 }
 
 import org.specs2.mutable._
+
 object LinkedListSpec extends Specification {
   "A newly initialized LinkedList " should {
     "have a size of 0" in {
       new LinkedList().size() must be_==(0)
+    }
+    "have no next element" in {
+      new LinkedList().next() must beNone
     }
   }
 }
