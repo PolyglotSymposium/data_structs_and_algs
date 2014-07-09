50df50e15d8ffde38b0508e5f614a844cb3b6821
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-08 23:10:19 -0400

    Refactored specs

--- the_code/linked_list/src/test/scala/poly/glot/linked_list/LinkedListSpec.scala
+++ the_code/linked_list/src/test/scala/poly/glot/linked_list/LinkedListSpec.scala
@@ -17,20 +17,24 @@ class LinkedList() {
 import org.specs2.mutable._
 
 object LinkedLstSpec extends Specification {
-  "A newly initialized LinkedList " should {
+  "A newly initialized LinkedList" should {
+    val subject = new LinkedList()
+
     "have a size of 1" in {
-      new LinkedList().size() must be_==(1)
+      subject.size() must be_==(1)
     }
     "have no next element" in {
-      new LinkedList().next() must beNone
+      subject.next() must beNone
     }
   }
 
   "A linked list with an element added to it" should {
+    val subject = new LinkedList()
+    val addedElement = new LinkedList()
+    subject.add(addedElement)
+
     "have a size of 2" in {
-      val first = new LinkedList()
-      first.add(new LinkedList())
-      first.size() must be_==(2)
+      subject.size() must be_==(2)
     }
   }
 }
