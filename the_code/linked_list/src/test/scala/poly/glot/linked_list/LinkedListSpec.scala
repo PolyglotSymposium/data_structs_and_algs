4f97cdd52ca76c84f99eb0efcd8562b1d0955441
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-08 23:03:12 -0400

    Added counter for number of elements

--- the_code/linked_list/src/test/scala/poly/glot/linked_list/LinkedListSpec.scala
+++ the_code/linked_list/src/test/scala/poly/glot/linked_list/LinkedListSpec.scala
@@ -1,10 +1,16 @@
 class LinkedList() {
+  private var elementCount = 1
+
   def size() : Int = {
-    return 1
+    return elementCount
   }
 
   def next() : Option[LinkedList] = {
     return None
+  }
+
+  def add(next : LinkedList) = {
+    elementCount += 1
   }
 }
 
@@ -17,6 +23,14 @@ object LinkedLstSpec extends Specification {
     }
     "have no next element" in {
       new LinkedList().next() must beNone
+    }
+  }
+
+  "A linked list with an element added to it" should {
+    "have a size of 2" in {
+      val first = new LinkedList()
+      first.add(new LinkedList())
+      first.size() must be_==(2)
     }
   }
 }
