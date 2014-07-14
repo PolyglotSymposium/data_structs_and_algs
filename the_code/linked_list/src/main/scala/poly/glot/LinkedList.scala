857b3bf976c806ac1027c4624338f9f01b5f80e6
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-14 18:39:59 -0400

    Slight clean-up...
    Just so I can feel like I did something after that unsuccessful attempt
    at making a `toList`

--- the_code/linked_list/src/main/scala/poly/glot/LinkedList.scala
+++ the_code/linked_list/src/main/scala/poly/glot/LinkedList.scala
@@ -4,11 +4,9 @@ class LinkedList[T]() {
   private var nextElement: Option[LinkedList[T]] = None
   private var datum: T = _
 
-  def size(): Int = {
-    nextElement match {
-      case None => 1
-      case Some(next) => 1 + next.size()
-    }
+  def size(): Int = nextElement match {
+    case None => 1
+    case Some(next) => 1 + next.size()
   }
 
   def data(): T = {
