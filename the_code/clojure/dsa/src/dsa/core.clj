92b92cfe7e4009e9084b57bd173892c602c89650
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-12 22:31:52 -0400

    First test on quick sort

--- the_code/clojure/dsa/src/dsa/core.clj
+++ the_code/clojure/dsa/src/dsa/core.clj
@@ -1,6 +1,9 @@
 (ns dsa.core
   (:gen-class))
 
+(defn qsort [xs]
+  [])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- the_code/clojure/dsa/test/dsa/core_test.clj
+++ the_code/clojure/dsa/test/dsa/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [dsa.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest sorted-empty-list-is-empty
+  (testing "sorting an empty list"
+    (is (= (qsort []) []))))
