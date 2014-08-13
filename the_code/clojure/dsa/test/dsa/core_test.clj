1ca78a90d4a71ddbe16b4e0567cfe269928f6bfe
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-12 22:35:51 -0400

    Third test: already sorted list with two elements

--- the_code/clojure/dsa/test/dsa/core_test.clj
+++ the_code/clojure/dsa/test/dsa/core_test.clj
@@ -9,3 +9,7 @@
 (deftest sorted-single-element-list-is-same
   (testing "sorting a list with one element"
     (is (= (qsort [42]) [42]))))
+
+(deftest sorted-two-already-sorted-elements
+  (testing "sorting two already sorted elements"
+    (is (= (qsort [42 43]) [42 43]))))
