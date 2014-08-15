b018e1e1b0907d26cb1427a711ba0d1583797245
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-15 12:03:59 -0400

    Added a cons operator

--- the_code/clojure/dsa/src/dsa/core.clj
+++ the_code/clojure/dsa/src/dsa/core.clj
@@ -2,7 +2,9 @@
   (:gen-class))
 
 (defrecord MyList [next data])
+
 (defn newlist [x] (MyList. :endoflist x))
+(defn mycons [elem otherlist] (MyList. otherlist elem))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- the_code/clojure/dsa/test/dsa/core_test.clj
+++ the_code/clojure/dsa/test/dsa/core_test.clj
@@ -9,3 +9,7 @@
 (deftest one-element-list-has-set-data-as-data
   (testing "one element list has endoflist as next"
    (is (= (:data (newlist 42))) 42)))
+
+(deftest when-cons-on-one-element-list-data-of-next-is-original
+  (testing "cons onto list with data, :next :data"
+   (is (= (:next (:data (mycons 35 (newlist 42))))) 42)))
