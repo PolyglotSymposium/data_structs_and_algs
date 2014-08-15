b0fbf12424d81dce0242557b95ae9cc52a6ca730
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-15 11:45:03 -0400

    Deleted the qsort stuff

--- the_code/clojure/dsa/src/dsa/core.clj
+++ the_code/clojure/dsa/src/dsa/core.clj
@@ -4,11 +4,6 @@
 (defrecord MyList [next])
 (def emptylist (MyList. :endoflist))
 
-(defn qsort [xs]
-  (if (< 2 (count xs)) xs
-  (let [x (first xs) r (rest xs)]
-    (if (> x (first r)) (conj r x) xs))))
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
